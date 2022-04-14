b93ed50c55355debf6e07e21ef302b49a9cf3b87
Justin Henderson <jus10henderson@gmail.com>
2022-04-14 07:59:15 -0500

    Add start new game
    Added the ability for players to start new game

--- Project2/CowsandBulls/CowsandBulls/ViewController.swift
+++ Project2/CowsandBulls/CowsandBulls/ViewController.swift
@@ -7,12 +7,15 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+var answer = ""
+var guesses = [String] ()
 
+class ViewController: NSViewController, NSTableViewDataSource ,NSTableViewDelegate {
     @IBOutlet var guess: NSTextField!
     @IBOutlet var tableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewGame()
 
         // Do any additional setup after loading the view.
     }
@@ -25,6 +28,63 @@ class ViewController: NSViewController {
         }
     }
 
+    //returns the number of items in guesses array
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
+    }
+    
+    //result calculation
+    func result(for guess: String) -> String {
+        var bulls = 0
+        var cows = 0
+        
+        let guessLetters = Array(guess)
+        let answerLetters = Array(answer)
+        
+        //loop through individual characters in guess array
+        for (index, letter) in guessLetters.enumerated() {
+            if letter == answerLetters[index]{
+                bulls += 1
+            } else if answerLetters.contains(letter) {
+                cows += 1
+            }
+        }
+        return "\(bulls)b \(cows)c"
+    }
+    
+    //add viewFor method..checks title of column to determine what data to show
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+            guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+            if tableColumn?.title == "Guess" {
+                // this is the "Guess" column; show a previous guess
+                vw.textField?.stringValue = guesses[row]
+            } else {
+                // this is the "Result" column; call our new method
+                vw.textField?.stringValue = result(for: guesses[row])
+            }
 
-}
+            return vw
+        }
+    // make table rows unselectable
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return false
+        }
+    
+    //Start new game. Method takes a player guess and clears the guess array when a new game is started
+    func startNewGame(){
+        guess.stringValue = ""
+        guesses.removeAll()
+        answer = ""
+        
+        //generate random numbers
+        var  numbers = Array(0...9)
+        numbers.shuffle()
+        
+        for _ in 0 ..< 4 {
+            answer.append(String(numbers.removeLast()))
+        }
+        tableView.reloadData()
+        }
+    }
 
